75ee4d24caa1dae1bdf4a47162f62a499101427c
JesterCharles <JesterCharles@gmail.com>
2022-07-27 12:24:37 -0400

    made another simple update to our terminal output

--- src/com/revature/nabnak/MainDriver.java
+++ src/com/revature/nabnak/MainDriver.java
@@ -4,5 +4,6 @@ public class MainDriver {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Simple change");
+        System.out.println("Another simple update");
     }
 }
