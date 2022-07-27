97ef02fa91004e26e1e9d973593dcb0ccef48ac0
JesterCharles <JesterCharles@gmail.com>
2022-07-27 12:08:07 -0400

    updated maindriver with a simple change

--- src/com/revature/nabnak/MainDriver.java
+++ src/com/revature/nabnak/MainDriver.java
@@ -3,5 +3,6 @@ package com.revature.nabnak;
 public class MainDriver {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Simple change");
     }
 }
