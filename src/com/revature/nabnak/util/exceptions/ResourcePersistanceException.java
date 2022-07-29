1a53b71202f12fbbcf50a459146336ac22136052
JesterCharles <JesterCharles@gmail.com>
2022-07-29 01:52:11 -0400

    made custom exceptions for several use cases

---
+++ src/com/revature/nabnak/util/exceptions/ResourcePersistanceException.java
@@ -0,0 +1,8 @@
+package com.revature.nabnak.util.exceptions;
+
+public class ResourcePersistanceException extends RuntimeException {
+
+    public ResourcePersistanceException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/revature/nabnak/util/exceptions/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.revature.nabnak.util.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
